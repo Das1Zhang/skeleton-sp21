d09961f234b843e977a60ee62f379f54b0178a08
Das1Zhang <1052094815@qq.com>
2025-07-05 20:54:11 +0800

    done lab3

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -48,4 +48,75 @@ public class TestBuggyAList {
             }
         }
     }
+
+    @Test
+    public void randomizedFunctionCalls1(){
+        AListNoResizing<Integer> L = new AListNoResizing<>();
+
+        int N = 500;
+        for(int i = 0; i < N; i += 1){
+            int operationNumber = StdRandom.uniform(0,3);
+            if(operationNumber == 0){
+                int randVal = StdRandom.uniform(0,100);
+                L.addLast(randVal);
+                System.out.println("addLast(" + randVal + ")");
+            } else if(operationNumber == 1){
+                int size = L.size();
+                if(size == 0){
+                    System.out.println("size: " + size + ", can't getLast().");
+                    continue;
+                }
+                int last = L.getLast();
+                System.out.println("getLast(): " + last);
+            } else if(operationNumber == 2){
+                int size = L.size();
+                if(size == 0){
+                    System.out.println("size: " + size + ", can't removeLast.");
+                    continue;
+                }
+                int last = L.removeLast();
+                System.out.println("removeLast(): " + last);
+            }
+        }
+    }
+
+    @Test
+    public void randomizedTest(){
+        AListNoResizing<Integer> correct = new AListNoResizing<>();
+        BuggyAList<Integer> buggy = new BuggyAList<>();
+        int N = 50000;
+        for(int i = 0; i < N; i += 1){
+            int operationNumber = StdRandom.uniform(0, 4);
+            if(operationNumber == 0){
+                int randVal = StdRandom.uniform(0, 100);
+                correct.addLast(randVal);
+                buggy.addLast(randVal);
+                assertEquals(correct.size(), buggy.size());
+                System.out.println("addLast(" + randVal + ")");
+            } else if(operationNumber == 1){
+                int size = buggy.size();
+                System.out.println("size: " + size);
+            } else if(operationNumber == 2){
+                int size = buggy.size();
+                if(size == 0){
+                    System.out.println("size: " + size + ", can't getLast().");
+                    continue;
+                }
+                int last_correct = correct.getLast();
+                int last_buggy = buggy.getLast();
+                assertEquals(last_correct, last_buggy);
+                System.out.println("getLast(): " + last_correct);
+            } else if(operationNumber == 3){
+                int size = buggy.size();
+                if(size == 0){
+                    System.out.println("size: " + size + ", can't removeLast().");
+                    continue;
+                }
+                int last_correct = correct.removeLast();
+                int last_buggy = buggy.removeLast();
+                assertEquals(last_correct, last_buggy);
+                System.out.println("removeLast(): " + last_correct);
+            }
+        }
+    }
 }
